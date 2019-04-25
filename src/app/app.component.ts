c6485da95d7fcd6f4a1df790a256f918cf53d8e2
Anthony <anthony.marque@gmail.com>
2019-04-25 14:00:14 +0200

    added buttons and serverService calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
-import { ServerService } from './server.service';
+import { Component } from "@angular/core";
+import { ServerService } from "./server.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   servers = [
     {
-      name: 'Testserver',
+      name: "Testserver",
       capacity: 10,
       id: this.generateId()
     },
     {
-      name: 'Liveserver',
+      name: "Liveserver",
       capacity: 100,
       id: this.generateId()
     }
   ];
-  constructor(private serverService: ServerService){}
+  constructor(private serverService: ServerService) {}
+
+  onGet() {
+    this.serverService.getServer()
+      .subscribe(
+        response => {
+          const data = response.json();
+          console.log(data);
+          this.onAddServer(data);
+          // this.servers.push({
+          //   name: response.name,
+          //   capacity: response.capacity,
+          //   id: response.id
+          // });
+        },
+        error => console.log(error)
+      );
+  }
+
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -28,11 +46,12 @@ export class AppComponent {
     });
   }
   onSave() {
-    this.serverService.storeServer(this.servers)
-    .subscribe(
-      response => console.log(response),
-      error => console.log(error)
-    );
+    this.serverService
+      .storeServer(this.servers)
+      .subscribe(
+        response => console.log(response),
+        error => console.log(error)
+      );
   }
   private generateId() {
     return Math.round(Math.random() * 10000);
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -7,7 +7,14 @@ import { Http } from '@angular/http';
 export class ServerService {
 
   constructor(private http: Http) { }
+  // storeServer(server:any[]) {
+  //   return this.http.post('https://httplearning-be1e8.firebaseio.com/data.json',server);
+  // }
   storeServer(server:any[]) {
-    return this.http.post('https://httplearning-be1e8.firebaseio.com/data.json',server);
+    return this.http.put('https://httplearning-be1e8.firebaseio.com/data.json',server);
+  }
+
+  getServer() {
+    return this.http.get('https://httplearning-be1e8.firebaseio.com/data.json');
   }
 }
